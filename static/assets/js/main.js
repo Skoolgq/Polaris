390d130a2f1757745aa915a99e65e11a2b17946d
Russell2259 <russell2259@tuta.io>
2023-05-02 07:35:42 -0600

    save

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,31 +1,44 @@
-import * as theme from '/assets/js/themes.js';
+import Theme from '/assets/js/themes.js';
 /*Please link all javascript files here using import*/
 
-//Someone should put this code somewhere
-document.querySelectorAll('a').forEach(a => {
-    a.onclick = (e) => {
-        if (a.dataset.link !== 'true') {
-            e.preventDefault();
+console.log(Theme);
+
+fetch('/assets/misc/nav.html')
+    .then(res => res.text())
+    .then(content => {
+        document.body.insertAdjacentHTML('afterbegin', content);
+    }).catch(e => {
+        alert('Failed to load navbar');
+
+        if (confirm('Try again?')) location.reload();
+    })
+
+/*setTimeout(() => {
+    document.querySelectorAll('a').forEach(a => {
+        a.onclick = (e) => {
+            if (a.dataset.link !== 'true') {
+                e.preventDefault();
+            }
+
+            if (a.href.startsWith(location.origin)) {
+                if (window.location.href !== a.href) {
+                    fetch(a.href)
+                        .then(res => res.text())
+                        .then(content => {
+                            setTimeout(() => {
+                                window.history.pushState({}, '', a.href);
+
+                                document.documentElement.innerHTML = content;
+                            }, 500);
+                        }).catch(e => {
+                            a.setAttribute('data-link', 'true');
+                            a.click();
+                        });
+                }
+            } else {
+                a.setAttribute('data-link', 'true');
+                a.click();
+            }
         }
-
-        if (a.href.startsWith(location.origin)) {
-            fetch(a.href)
-                .then(res => res.text())
-                .then(content => {
-                    setTimeout(() => {
-                        console.log('a');
-
-                        window.history.pushState({}, '', a.href);
-
-                        document.documentElement.innerHTML = content;
-                    }, 500);
-                }).catch(e => {
-                    a.setAttribute('data-link', 'true');
-                    a.click();
-                });
-        } else {
-            a.setAttribute('data-link', 'true');
-            a.click();
-        }
-    }
-});
+    });
+}, 500)*/
