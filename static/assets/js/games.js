c590092ff6db9807f180ed77fc7d130d401e0c40
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-05-17 22:44:19 -0400

    Update games.js

--- static/assets/js/games.js
+++ static/assets/js/games.js
@@ -6,7 +6,7 @@ const tiltEffectSettings = {
   perspective: 1000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
   scale: 1.05, // transform scale - 2 = 200%, 1.5 = 150%, etc..
   speed: 800, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
-  easing: 'cubic-bezier(.03,.39,.52,.99)' // easing (transition-timing-function) of the enter/exit transition
+  
 };
 
 const load = () => {
