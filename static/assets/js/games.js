5df47f1b7028ef68ea0ff17d918cf92e3f9cd9b7
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-05-17 22:48:55 -0400

    Update games.js

--- static/assets/js/games.js
+++ static/assets/js/games.js
@@ -2,11 +2,11 @@ import PolarisError from './error.js';
 import frame from './frame.js';
 
 const tiltEffectSettings = {
-  max: 10, // max tilt rotation (degrees (deg))
+  max: 15, // max tilt rotation (degrees (deg))
   perspective: 1000, // transform perspective, the lower the more extreme the tilt gets (pixels (px))
   scale: 1.05, // transform scale - 2 = 200%, 1.5 = 150%, etc..
   speed: 800, // speed (transition-duration) of the enter/exit transition (milliseconds (ms))
-  
+  easing: 'cubic-bezier(.03,.98,.52,.99)' // easing (transition-timing-function) of the enter/exit transition
 };
 
 const load = () => {
