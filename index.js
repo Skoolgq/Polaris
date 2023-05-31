78b74ac46f47ccadafbdb7683eaa246a59307d8a
Russell2259 <russell2259@tuta.io>
2023-05-31 16:52:09 -0600

    use easyviolet

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-import http from 'node:http';
+import Easyviolet from 'easyviolet';
 import express from 'express';
-import * as url from 'url';
-import * as path from 'node:path';
-
-//Import proxy scripts
-import createBareServer from '@tomphttp/bare-server-node';
-import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
+import url from 'url';
+import path from 'node:path';
 
 const app = express();
-const server = http.createServer();
-const bare = createBareServer('/bare/');
+const ultraviolet = new Easyviolet();
 const port = process.env.PORT || process.argv[2] || 8080;
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
+app.use(ultraviolet.express(app));
 app.use(express.static(path.join(__dirname, '/static'), { extensions: ['html'] }));
-app.use('/uv/', express.static(uvPath));
 app.use((req, res) => res.status(404).sendFile(path.join(__dirname, './static/', '404.html')));
 app.use((e, req, res, next) => res.status(500).send(`Something Broke \n\n The error was: ${e.stack}`));
 
-server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeRequest(req, res);
-    } else {
-        app(req, res);
-    }
-});
-
-server.on('upgrade', (req, res) => {
-    if (bare.shouldRoute(req)) {
-        bare.routeUpgrade(req, socket, head);
-    } else {
-        socket.end();
-    }
-});
-
-server.listen(port, '0.0.0.0', () => {
+const server = app.listen(port, '0.0.0.0', () => {
     console.log(`Polaris is running on port ${server.address().port}, using nodejs ${process.version}`);
 });
