26b93e29939d4dbf6fb4334ff195a40d9423ca38
fowntain <dbjones3@students.wcpss.net>
2023-07-27 14:09:01 -0400

    undo i broke the whole thing

--- static/assets/js/frame.js
+++ static/assets/js/frame.js
@@ -1,14 +1,13 @@
 /*const gamedata = JSON.parse(localStorage.getItem('gamedata'));
 
-    document.getElementById('logo-options').textContent = gamedata.name;
-    document.getElementById('game-icon-sm').src = gamedata.imgurl;
-    document.getElementById('game-frame').src = gamedata.gameurl;*/
+document.getElementById('logo-options').textContent = gamedata.name;
+document.getElementById('game-icon-sm').src = gamedata.imgurl;
+document.getElementById('game-frame').src = gamedata.gameurl;*/
 
-    const load = (url, name, type) => {
-        document.addEventListener('keydown', (e) => {
-            if (e.key === 'f') fullscreen();
-        });
-    };
+const load = (url, name, type) => {
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'f') fullscreen();
+    });
 
     function fullscreen() {
         let iframe = document.getElementById('game-frame');
@@ -18,7 +17,7 @@
         else if (iframe.webkitRequestFullscreen) iframe.webkitRequestFullscreen();
         else if (iframe.mozRequestFullScreen) iframe.mozRequestFullScreen();
         else if (iframe.msRequestFullscreen) iframe.msRequestFullscreen();
-    }
+    };
+};
 
-    document.querySelector('.fa-expand').addEventListener('click', fullscreen);
-    export default { load, fullscreen };
+export default { load };
