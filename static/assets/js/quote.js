ed7b83ebd74a7c57c018c4821fd0382723eaf80e
fowntain <dbjones3@students.wcpss.net>
2023-08-09 16:16:45 -0400

    add tons more quotes

--- static/assets/js/quote.js
+++ static/assets/js/quote.js
@@ -10,7 +10,22 @@ var sayings = [
     "tucker's the best",
     "please help",
     "insert nice quote",
-    "JOIN THE SERVER OFHORFHRUHOIF"
+    "JOIN THE SERVER OFHORFHRUHOIF",
+    "wassup",
+    "The names Bond, names James",
+    "just existing yk?",
+    "ew dreambox sounds suck",
+    "if do you like you know? me too",
+    "dm fowntain if you saw this",
+    "dean was here",
+    "russell is sooper koool",
+    "shoutout to my admins :)",
+    "I see you, RAT. YEAH, YOU",
+    "Shoutout to rolesville",
+    "shoutout to carroll i see you",
+    "sup xordas",
+    "hey ms pierre, you suck",
+    "'pisshole' - rat, 2023"
   ];
 
   // Function to set random saying as homequote text
