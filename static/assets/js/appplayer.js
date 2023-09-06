20fa585863589eceaf63cee844d9bc76a945e83a
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-09-06 08:38:23 -0400

    Update appplayer.js

--- static/assets/js/appplayer.js
+++ static/assets/js/appplayer.js
@@ -1,10 +1,4 @@
 const load = () => {
-            if (iframe.requestFullscreen) iframe.requestFullscreen();
-            else if (iframe.webkitRequestFullscreen) iframe.webkitRequestFullscreen();
-            else if (iframe.mozRequestFullScreen) iframe.mozRequestFullScreen();
-            else if (iframe.msRequestFullscreen) iframe.msRequestFullscreen();
-        }
-    });
 
 
  fetch('/assets/JSON/apps.json')
