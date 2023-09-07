ac2e5ce1fe1a74aba44a18760b9a24b5061494a8
SkoolBot <141183872+SkoolBot@users.noreply.github.com>
2023-09-07 12:52:37 -0400

    Change featured game to 1 via admin panel

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -121,7 +121,7 @@ if (location.pathname === '/') {
     fetch('/assets/JSON/games.json')
         .then(res => res.json())
         .then(games => {
-            const randomID = 40; // :3
+            const randomID = 1; // :3
             const game = games[randomID];
 
             document.querySelector('.featuredimg').addEventListener('click', function() {
