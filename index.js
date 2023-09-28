1e1f7812a56218b07e67f5964fa82b88c599597c
Cobalt-60 <95923271+plastics-eater@users.noreply.github.com>
2023-09-28 19:19:46 -0400

    revert

--- index.js
+++ index.js
@@ -1,53 +1,61 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
-import { createServer } from "node:http";
-import express from 'express';
-import path from 'node:path';
+import Fastify from 'fastify';
+import fastifyStatic from '@fastify/static';
+import fastifyCompress from '@fastify/compress';
+import { createBareServer } from '@tomphttp/bare-server-node';
+import { createServer } from 'http';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
 import mime from 'mime';
-import cors from 'cors';
-import url from 'url';
 
-const app = express();
-const server = createServer();
-const bare = createBareServer("/bare/");
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = process.env.PORT || process.argv[2] || 8080;
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-app.use(express.static(path.join(__dirname, '/static'), { extensions: ['html'] }));
+const bare = createBareServer('/bare/');
+const serverFactory = (handler) => {
+    return createServer().on("request", (req, res) => {
+        if (bare.shouldRoute(req)) bare.routeRequest(req, res);
+        else handler(req, res);
+    }).on("upgrade", (req, socket, head) => {
+        if (bare.shouldRoute(req)) bare.routeUpgrade(req, socket, head);
+        else socket.end();
+    });
+};
 
-app.get('/cdn/*', cors({ origin: false }), async (req, res, next) => {
-    let reqTarget = `https://raw.githubusercontent.com/Skoolgq/Polaris-Assets/main/${req.path.replace('/cdn/', '')}`;
+const fastify = Fastify({
+    serverFactory
+});
+
+fastify.register(fastifyStatic, {
+    root: join(__dirname, '/static'),
+    extensions: ['html']
+});
+
+fastify.register(fastifyCompress, {
+    encodings: ["br"]
+});
+
+fastify.setNotFoundHandler((request, reply) => {
+    const notFoundFile = readFileSync('./static/404.html');
+    reply.type('text/html').send(notFoundFile);
+});
+
+fastify.get('/cdn/*', async (req, res) => {
+    let reqTarget = `https://raw.githubusercontent.com/Skoolgq/Polaris-Assets/main/${req.params['*']}`;
     
     const asset = await fetch(reqTarget);
     if (asset.status == 200) {
         var data = Buffer.from(await asset.arrayBuffer());
         
         const noRewrite = ['.unityweb'];
-        if (!noRewrite.includes(mime.getExtension(reqTarget))) res.writeHead(200, {
-            'content-type': mime.getType(reqTarget)
-        });
+        if (!noRewrite.includes(mime.getExtension(reqTarget))) res.type(mime.getType(reqTarget));
 
-        if (mime.getType(reqTarget) === 'text/html') data = data + '<script src="/assets/js/cdn_inject.js" preload="true"></script>';
-
-        res.end(data);
-    } else next();
+        if (mime.getType(reqTarget) === 'text/html') data = Buffer.concat([data, Buffer.from('<script src="/assets/js/cdn_inject.js" preload="true"></script>')]);
+        res.send(data);
+    } else res.callNotFound();
 });
 
-server.on("request", (req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-server.listen({
+fastify.listen({
     port,
-  });
+    host: process.env.NODE_VERSION ? `0.0.0.0` : `localhost` // for render
+}, async () => console.log(`Polaris has started!\n   http://localhost:8080`));
