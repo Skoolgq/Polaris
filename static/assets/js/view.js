f2fb6bc2a774e3fcdf080867edfd321de77e35d0
Russell2259 <russell2259@tuta.io>
2024-01-03 15:25:01 +0000

    fix game loading issue

--- static/assets/js/view.js
+++ static/assets/js/view.js
@@ -16,41 +16,41 @@ if (params.get('load')) {
             if (parsedData.proxied) {
                 await loadProxyWorker('uv');
 
-                document.querySelector('iframe').src = '/uv/service/' + encoder['xor'].encode(parsedData.target);
-            } else document.querySelector('iframe').src = parsedData.target;
+                document.querySelector('#frame').src = '/uv/service/' + encoder['xor'].encode(parsedData.target);
+            } else document.querySelector('#frame').src = parsedData.target;
 
-            document.querySelector('iframe').addEventListener('load', () => {
+            document.querySelector('#frame').addEventListener('load', () => {
                 document.querySelector('.title').textContent = parsedData.title;
 
-                document.querySelector('iframe').style.transition = 'none';
-                document.querySelector('iframe').style.background = '#fff';
-                document.querySelector('iframe').contentWindow.addEventListener('mouseover', () => {
+                document.querySelector('#frame').style.transition = 'none';
+                document.querySelector('#frame').style.background = '#fff';
+                document.querySelector('#frame').contentWindow.addEventListener('mouseover', () => {
                     document.querySelector('.gamebar').classList.add('collapsed');
                     document.querySelector('.hitbox').classList.remove('active');
                 });
-                document.querySelector('iframe').contentWindow.addEventListener('mouseout', () => {
+                document.querySelector('#frame').contentWindow.addEventListener('mouseout', () => {
                     document.querySelector('.gamebar').classList.remove('collapsed');
                     document.querySelector('.hitbox').classList.add('active');
                 });
 
-                if (document.querySelector('iframe').matches(':hover')) setTimeout(() => {
+                if (document.querySelector('#frame').matches(':hover')) setTimeout(() => {
                     document.querySelector('.gamebar').classList.remove('collapsed');
                     document.querySelector('.hitbox').classList.add('active');
                 }, 1000);
             });
 
             document.querySelector('#fullscreen').addEventListener('click', () => {
-                const iframe = document.querySelector('iframe');
+                const frame = document.querySelector('#frame');
 
-                if (iframe.requestFullscreen) iframe.requestFullscreen();
-                else if (iframe.webkitRequestFullscreen) iframe.webkitRequestFullscreen();
-                else if (iframe.mozRequestFullScreen) iframe.mozRequestFullScreen();
-                else if (iframe.msRequestFullscreen) iframe.msRequestFullscreen();
+                if (frame.requestFullscreen) frame.requestFullscreen();
+                else if (frame.webkitRequestFullscreen) frame.webkitRequestFullscreen();
+                else if (frame.mozRequestFullScreen) frame.mozRequestFullScreen();
+                else if (frame.msRequestFullscreen) frame.msRequestFullscreen();
             });
 
             window.addEventListener('fullscreenchange', () => {
-                if (document.fullscreenElement) document.querySelector('iframe').style.borderRadius = '0px';
-                else document.querySelector('iframe').style.borderRadius = '';
+                if (document.fullscreenElement) document.querySelector('#frame').style.borderRadius = '0px';
+                else document.querySelector('#frame').style.borderRadius = '';
             });
 
             document.querySelector('#return').addEventListener('click', () => {
