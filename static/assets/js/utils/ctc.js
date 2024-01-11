63aec672439ff6f7ef5983bd0d87b9ff0113af48
Russell2259 <russell2259@tuta.io>
2024-01-11 23:56:31 +0000

    work on ctc system and remove unused files

--- static/assets/js/utils/ctc.js
+++ static/assets/js/utils/ctc.js
@@ -1,11 +1,44 @@
-import { uuid } from '../utils.js';
+import { uuid, EventEmitter } from '../utils.js';
 
 const ctc = {
-    connect: (pageID) => {
-        sessionStorage.getItem('ctc_' + pageID);
+    /**
+     * Check if a tab exists
+     * @param {string} pageID 
+     * @returns {boolean}
+     */
+    exists: (pageID) => Object.keys(sessionStorage).includes('ctc_' + pageID),
+    connect: (remoteID, currentID) => new Promise((resolve, reject) => {
+        if (ctc.exists(remoteID)) {
+            sessionStorage.setItem('ctc_' + remoteID, currentID);
 
-        sessionStorage['ctc_' + pageID]
-    },
+            const events = new EventEmitter();
+            const channel = 'ctc_connection' + currentID + '>' + remoteID;
+            var prev = sessionStorage.getItem(channel);
+            
+            const listener = setInterval(() => {
+                if (sessionStorage.getItem(channel) === 'ctc:disconnect') {
+                    sessionStorage.setItem(channel, '');
+                    events.emit('disconnect');
+                    clearInterval(listener);
+                    
+                    return;
+                }
+                
+                if (prev !== sessionStorage.getItem(channel)) {
+                    prev = sessionStorage.getItem(channel);
+
+                    events.emit('message', sessionStorage.getItem(channel));
+                }
+            }, 10);
+
+            return {
+                disconnect: () => {
+                    clearInterval(listener);
+                    sessionStorage.setItem(channel, 'ctc:disconnect');
+                }
+            };
+        } else reject('Tab does not exist');
+    }),
     register: () => {
         const registrationData = sessionStorage.getItem('ctc_registration') ? JSON.parse(sessionStorage.getItem('ctc_registration')) : {};
         const pageID = uuid();
