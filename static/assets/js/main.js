60acc106be925ddbb7e345f284ec49c3db420b5f
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2024-01-23 20:45:29 -0700

    ._.

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -7,7 +7,7 @@ import Cheats from './cheats.js';
 import Games from './games.js';
 import Apps from './apps.js';
 
-const umami = loadAnalytics();
+const umami = await loadAnalytics();
 
 if (location.pathname !== '/view') loadSidebarInterface();
 loadEasterEggs();
