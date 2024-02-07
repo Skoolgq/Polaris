f91616a48ed4466756d3fb3a508b19df537ad203
Dean @ Skool <dbjones3@students.wcpss.net>
2024-02-07 14:01:22 -0500

    blacklist start

--- static/assets/js/search.js
+++ static/assets/js/search.js
@@ -1,11 +1,19 @@
 import { createViewPage, isValidURL } from './utils.js';
 
-const load = () => {
+const load = async () => {
     const form = document.querySelector('#wpf');
     const query = document.querySelector('#query');
+    const response = await fetch('./JSON/blacklist.json');
+    const blacklist = await response.json();
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
+
+        // blacklist stuff :)
+        if (containsBlacklistedTerm(query.value, blacklist)) {
+            alert('This search contains a blacklisted term. Please keep it appropriate, thanks!');
+            return;
+        }
 
         const url = isValidURL(query.value) ? ((!query.value.startsWith('http://') && !query.value.startsWith('https://')) ? 'https://' + query.value : query.value) : 'https://www.google.com/search?q=' + encodeURIComponent(query.value);
 
@@ -16,9 +24,14 @@ const load = () => {
         setTimeout(() => createViewPage({
             target: url,
             proxied: true,
-            title: 'Search Results'
+            title: 'Proxy'
         }), 500);
     });
+}
+
+// helper for blacklist
+const containsBlacklistedTerm = (value, blacklist) => {
+    return blacklist.some(entry => value.includes(entry));
 }
 
 export default { load };
