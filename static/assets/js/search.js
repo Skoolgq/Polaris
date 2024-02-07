35c98b0ae4d37009591f5150599b7ca3d9e6d48c
Dean @ Skool <dbjones3@students.wcpss.net>
2024-02-07 14:26:02 -0500

    YES IT WORKS!!1!1!

--- static/assets/js/search.js
+++ static/assets/js/search.js
@@ -3,33 +3,31 @@ import { createViewPage, isValidURL } from './utils.js';
 const load = async () => {
     const form = document.querySelector('#wpf');
     const query = document.querySelector('#query');
-    const response = await fetch('./JSON/blacklist.json');
+    const response = await fetch('./assets/JSON/blacklist.json');
     const blacklist = await response.json();
 
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
-
-        // blacklist stuff :)
-        if (containsBlacklistedTerm(query.value, blacklist)) {
-            alert('This search contains a blacklisted term. Please keep it appropriate, thanks!');
-            return;
-        }
 
         const url = isValidURL(query.value) ? ((!query.value.startsWith('http://') && !query.value.startsWith('https://')) ? 'https://' + query.value : query.value) : 'https://www.google.com/search?q=' + encodeURIComponent(query.value);
 
         document.body.style.opacity = '0.7';
 
         umami.track('query-' + query.value);
-
+        
+        if (containsBlacklistedTerm(query.value, blacklist)) {
+            alert("You're downright weird.\nThis search contains a blacklisted term.");
+            return;
+        } else {
         setTimeout(() => createViewPage({
             target: url,
             proxied: true,
-            title: 'Proxy'
+            title: 'Search Results'
         }), 500);
+        }
     });
 }
 
-// helper for blacklist
 const containsBlacklistedTerm = (value, blacklist) => {
     return blacklist.some(entry => value.includes(entry));
 }
